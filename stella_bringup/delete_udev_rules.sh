71dd60d44c65963ec49c2dfc577b5d7bf810e73b
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-07-13 17:10:57 +0900

    Add files via upload

--- stella_bringup/delete_udev_rules.sh
+++ stella_bringup/delete_udev_rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "delete remap the devices serial port(ttyUSBX,ttySX) to  ydlidar, Astra Pro, AHRS, Motordriver, Bluetooth"
+echo "delete remap the devices serial port(ttyUSBX,ttySX,videoX) to  ydlidar, WLKATA, AHRS, Motordriver, Bluetooth, C920"
 echo "sudo rm   /etc/udev/rules.d/stella.rules"
 sudo rm   /etc/udev/rules.d/stella.rules
 echo " "
