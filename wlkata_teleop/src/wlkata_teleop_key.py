70233b14c524cce4dd2eafdf490808f9305bfc62
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-07-13 15:35:55 +0900

    Add files via upload

---
+++ wlkata_teleop/src/wlkata_teleop_key.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Bool
+import sys, select, termios, tty
+
+def get_key():
+	tty.setraw(sys.stdin.fileno())
+	rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+	if rlist:
+		key = sys.stdin.read(1)
+	else:
+		key = ''
+
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+	return key
+
+
+
+if __name__=="__main__":
+	settings = termios.tcgetattr(sys.stdin)
+
+	rospy.init_node('pose_node')
+	pub_pose = rospy.Publisher('pose',Pose,queue_size=10)
+	pub_gripper = rospy.Publisher('gripper',Bool,queue_size=10)
+	rate = rospy.Rate(10)
+	p = Pose()
+	p.position.x = 198.6
+	p.position.y = 0.0
+	p.position.z = 230.4
+	g = Bool()
+
+	while(1):
+		key = get_key()
+		if key == 'w' :
+			p.position.x = p.position.x + 5.0
+			pub_pose.publish(p)
+		elif key == "x" :
+			p.position.x = p.position.x - 5.0
+			pub_pose.publish(p)
+		elif key == "a" :
+			p.position.y = p.position.y + 5.0
+			pub_pose.publish(p)
+		elif key == "d" :
+			p.position.y = p.position.y - 5.0
+			pub_pose.publish(p)
+		elif key == "e" :
+			p.position.z = p.position.z + 5.0
+			pub_pose.publish(p)
+		elif key == "c" :
+			p.position.z = p.position.z - 5.0
+			pub_pose.publish(p)
+		elif key == "h" :
+			p.position.x = 198.6
+			p.position.y = 0.0
+			p.position.z = 230.4
+			pub_pose.publish(p)
+		elif key == '.':
+			g.data = True
+			pub_gripper.publish(g)
+		elif key == '/':
+			g.data = False
+			pub_gripper.publish(g)
+
+		else:
+			if key == '\x03':
+				break
+		
+		rate.sleep()
